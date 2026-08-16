928ca446d82e7fcad318037cc9bea45e3d47d568
maheshkumar <maheshkumar30102@gmail.com>
2026-08-16 10:23:46 +0530

    Configure Microsoft Clarity cookie consent scoping

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -39,6 +39,7 @@ export default function Analytics() {
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "${clarityId}");
+            window.clarity("consent");
           `}
         </Script>
       )}
